aa349e6d4bf444220cdfc139e69ac0cb7eba9e21
DDarrow123 <ddarrowphl@gmail.com>
2019-02-24 16:16:52 -0500

    fixed side nav bar

--- Example_1/src/index.js
+++ Example_1/src/index.js
@@ -1,34 +1,48 @@
 const navContainer = document.querySelector(".nav-container");
 const siteName = document.querySelector(".site-name");
+const leftSideNavContainer = document.querySelector(".side-nav-container");
 
 navContainer.addEventListener("mouseover", event => {
   console.log(event.target);
-  // debugger;
-  if (
-    event.target.children[0] &&
-    event.target.children[0].className &&
-    event.target.children[0].className === "nav-link"
-  ) {
+  const target = event.target.children[0];
+  // const targetParent = event.target.parentElement.children[0];
+
+  if (target && target.className && target.className === "nav-link") {
     event.target.style.textDecorationLine = "underline";
   }
   if (event.target.className === "nav-cart") {
     console.log(event.target);
     event.target.style.textDecorationLine = "underline";
+    event.target.style.textDecorationColor = "red";
   }
   if (
-    event.target.children[0] &&
-    event.target.children[0].className &&
-    event.target.children[0].className === "site-name"
+    event.target &&
+    event.target.className &&
+    event.target.className === "oval-image"
   ) {
     console.log(event.target);
     // debugger;
-    // const siteName = document.querySelector(".site-name");
     siteName.innerHTML = "Hello!";
   }
 });
 
 navContainer.addEventListener("mouseout", event => {
-  // navSiteName.innerHTML = "avini<span>&#248</span>n";
   event.target.style.textDecoration = "none";
   siteName.innerHTML = "avini<span>&#248</span>n";
+});
+
+leftSideNavContainer.addEventListener("mouseover", event => {
+  console.log(event.target);
+  if (
+    event.target &&
+    event.target.className &&
+    event.target.className === "side-list-item"
+  ) {
+    event.target.style.textDecorationLine = "underline";
+    event.target.style.textDecorationStyle = "wavy";
+  }
+});
+
+leftSideNavContainer.addEventListener("mouseout", event => {
+  event.target.style.textDecorationLine = "none";
 });
