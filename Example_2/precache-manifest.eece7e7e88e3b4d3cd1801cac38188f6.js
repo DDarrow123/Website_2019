ce3bac74dfd6c707b5ef7b9b7a20879c8bbbdbbf
DDarrow123 <ddarrowphl@gmail.com>
2019-02-24 10:57:18 -0500

    added static build of festival page to website

---
+++ Example_2/precache-manifest.eece7e7e88e3b4d3cd1801cac38188f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1401b72ffabc82f84ded",
+    "url": "/static/css/main.4049fabb.chunk.css"
+  },
+  {
+    "revision": "1401b72ffabc82f84ded",
+    "url": "/static/js/main.1401b72f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6ea6c4b890d5bb62bafe",
+    "url": "/static/js/2.6ea6c4b8.chunk.js"
+  },
+  {
+    "revision": "6b78b3279398818a1cc3d03fbb5cf189",
+    "url": "/index.html"
+  }
+];
