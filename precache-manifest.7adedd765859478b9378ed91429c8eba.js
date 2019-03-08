34420064dbc4a522accd7a2304cfc09cf49bcdf6
DDarrow123 <ddarrowphl@gmail.com>
2019-03-07 20:38:09 -0500

    added new files to current website

---
+++ precache-manifest.7adedd765859478b9378ed91429c8eba.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "10d0dac472a5382de80e2866b058e541",
+    "url": "/static/media/github_icon.10d0dac4.svg"
+  },
+  {
+    "revision": "be88d2af94fa92153343",
+    "url": "/static/css/main.730b30b9.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "00acebe958dc1fa07f66",
+    "url": "/static/js/2.00acebe9.chunk.js"
+  },
+  {
+    "revision": "04c4147316628327084e907059454e94",
+    "url": "/static/media/play_icon.04c41473.svg"
+  },
+  {
+    "revision": "16c011064cbd91dc1791fbcbb1420654",
+    "url": "/static/media/restart_icon.16c01106.svg"
+  },
+  {
+    "revision": "0b2b710ae44e6f2a31fcacb54bf081af",
+    "url": "/static/media/pause_icon.0b2b710a.svg"
+  },
+  {
+    "revision": "746377b1ae08ae5e7ec9bea65263745a",
+    "url": "/static/media/black_white_img.746377b1.jpg"
+  },
+  {
+    "revision": "7daf763e8e6dc5178dee034695b50291",
+    "url": "/static/media/small_flower_img.7daf763e.jpg"
+  },
+  {
+    "revision": "900bf215b55d48993baa9051046f9e12",
+    "url": "/static/media/twitter_icon.900bf215.svg"
+  },
+  {
+    "revision": "a857427d7883f8210c2de4b8f302b372",
+    "url": "/static/media/insta_icon.a857427d.svg"
+  },
+  {
+    "revision": "be88d2af94fa92153343",
+    "url": "/static/js/main.be88d2af.chunk.js"
+  },
+  {
+    "revision": "c96c4bf03e4edaa323882b6e7fa15968",
+    "url": "/static/media/festival_img.c96c4bf0.png"
+  },
+  {
+    "revision": "d1f983991e0133bcb9da09cacea30211",
+    "url": "/static/media/beauty_img.d1f98399.png"
+  },
+  {
+    "revision": "44bd08b7a48a3371b596afa7726ee3d8",
+    "url": "/static/media/fashion_lister.44bd08b7.png"
+  },
+  {
+    "revision": "68b75e6b4709329e37feb93a839ede35",
+    "url": "/static/media/fashion_week.68b75e6b.png"
+  },
+  {
+    "revision": "1c1890ff5b97edb47b6b126aecbef54d",
+    "url": "/static/media/fright_matic.1c1890ff.png"
+  },
+  {
+    "revision": "be3c6b4bb1611e57b816057604c54d77",
+    "url": "/static/media/tall_flower_img.be3c6b4b.jpg"
+  },
+  {
+    "revision": "e2c1fe7ac8fe297efb616f0406d66f44",
+    "url": "/static/media/noun_circle.e2c1fe7a.svg"
+  },
+  {
+    "revision": "4e2b137ffda80e4227a8a7bea3000068",
+    "url": "/static/media/devon_img.4e2b137f.jpg"
+  },
+  {
+    "revision": "48fe6532aa4d3a242195a701066fec49",
+    "url": "/static/media/question_img.48fe6532.png"
+  },
+  {
+    "revision": "8c0f1925715a3d72135ac800d336b8e1",
+    "url": "/index.html"
+  }
+];
